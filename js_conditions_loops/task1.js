702414b4b70affe087c51a3dcb23f04ad87fd27f
Tanya <malceva.great@gmail.com>
2023-09-06 22:31:48 +0300

    Added directory js_conditions_loops Added files: task1.js task2.js task3.js

---
+++ js_conditions_loops/task1.js
@@ -0,0 +1,15 @@
+const averageGrade = 90;
+
+if (averageGrade <= 60) {
+    console.log ("Unsatisfactorily")
+} else if (averageGrade >= 60 && averageGrade <= 70) {
+    console.log("Satisfactorily")
+} else if (averageGrade >= 71 && averageGrade <= 80) {
+    console.log("Fine")
+} else if (averageGrade >= 81 && averageGrade <= 90) {
+    console.log ("Very good")
+} else if (averageGrade >= 91 && averageGrade <= 100) {
+    console.log("Perfectly")
+} else {
+    console.log("Invalid Grade")
+}
